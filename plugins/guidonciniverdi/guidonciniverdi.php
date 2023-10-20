f1df6860d794c150e9c0fa8a5ea924f9f13ce211
Alessandro Perlo <aleperlo002@gmail.com>
2023-10-20 09:00:39 +0200

    Primo commit

---
+++ plugins/guidonciniverdi/guidonciniverdi.php
@@ -0,0 +1,89 @@
+<?php
+
+/*
+ * Plugin Name: Guidoncini Verdi
+ */
+
+if( ! defined( 'ABSPATH' ) )
+    exit( 'Restricted access' );
+
+/*
+ * Restrizione degli utenti
+ */
+
+// Solo le squadriglie devono poter accedere alla media library
+// Permetti solo a editor e admin (blocca author e contributor)
+// di vedere contenuti non caricati da loro
+function guidoncini_show_current_sq_attachments( $query ) {
+    global $user_ID;
+    if ( ! current_user_can( 'edit_others_posts' ) ) {
+        $query['author'] = $user_ID;
+    }
+    return $query;
+}
+add_filter( 'ajax_query_attachments_args', 'guidoncini_show_current_sq_attachments' );
+function guidoncini_show_current_sq_attachments_list( $query ) {
+    global $user_ID;
+    if ( function_exists( 'get_current_screen')  ) { 
+	$screen = get_current_screen(); 
+    } 
+    if ( in_array( $screen->id, array( 'upload' ) ) ) {
+        if ( ! current_user_can( 'edit_others_posts' ) ) {
+            $query['author'] = $user_ID;
+        }
+    }
+    return $query;
+}
+add_filter( 'request', 'guidoncini_show_current_sq_attachments_list' );
+
+// Impedisci alle squadriglie di vedere articoli non creati da loro
+// Permetti solo a editor e admin (blocca author e contributor)
+// di vedere articoli non scritti da loro
+function guidoncini_show_current_sq_posts( $query ) {
+    global $pagenow, $user_ID;
+    if( 'edit.php' != $pagenow || !$query->is_admin )
+	return $query;
+    if( ! current_user_can( 'edit_others_posts' ) ) {
+	global $user_ID;
+	$query->set( 'author', $user_ID );
+    }
+    return $query;
+}
+add_filter( 'pre_get_posts', 'guidoncini_show_current_sq_posts' );
+
+// Impedisci alle squadriglie di eliminare post
+// Impedisci al ruolo author di eliminare post
+function guidoncini_disable_sq_post_deletion() {
+    $role = get_role( 'author' );
+    $role->remove_cap( 'delete_posts' );
+    $role->remove_cap( 'delete_published_posts' );
+}
+register_activation_hook( __FILE__, 'guidoncini_disable_sq_post_deletion' );
+function guidoncini_enable_sq_post_deletion() {
+    $role = get_role( 'author' );
+    $role->add_cap( 'delete_posts' );
+    $role->add_cap( 'delete_published_posts' );
+}
+register_deactivation_hook( __FILE__, 'guidoncini_enable_sq_post_deletion' );
+
+// Impedisci alle squadriglie di creare post
+// - Mappa la capability create_posts a create_posts nel post type 'post'
+// - Impedisci al ruolo di author di creare post
+function guidoncini_add_create_posts_cap( $args ) {
+    $args['map_meta_cap'] = true;
+    $args['capabilities']['create_posts'] = 'create_posts';
+    return $args;
+}
+add_filter( 'register_post_post_type_args', 'guidoncini_add_create_posts_cap' );
+function guidoncini_disable_sq_post_creation() {
+    $role = get_role( 'author' );
+    $role->remove_cap( 'create_posts' );
+}
+register_activation_hook( __FILE__, 'guidoncini_disable_sq_post_creation' );
+function guidoncini_enable_sq_post_creation() {
+    $role = get_role( 'author' );
+    $role->add_cap( 'create_posts' );
+}
+register_deactivation_hook( __FILE__, 'guidoncini_enable_sq_post_creation' );
+
+?>
---
+++ themes/guidonciniverdi/readme.txt
@@ -0,0 +1,38 @@
+=== Guidoncini Verdi ===
+Contributors: 
+Requires at least: 6.0
+Tested up to: 6.3.1
+Requires PHP: 5.7
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+Tema per il sito web \"Guidoncini Verdi Piemonte\"
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
+
+== Copyright ==
+
+Guidoncini Verdi WordPress Theme, (C) 2023 
+Guidoncini Verdi is distributed under the terms of the GNU GPL.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+Roboto Font
+Copyright 2018 The Roboto Slab Project Authors (https://github.com/googlefonts/robotoslab) 
+Licensed under the Apache License, Version 2.0 
+License URL: http://www.apache.org/licenses/LICENSE-2.0 
+Source: Google.com
+-- End of Roboto Font credits --
