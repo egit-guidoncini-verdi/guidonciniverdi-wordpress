bfb66b24a39b73e51ac9a2f521da51ea491fa03e
Gioele Cerri <gioele@cerri.es>
2024-01-27 17:03:20 +0100

    Use h1 for squadriglia block

--- plugins/guidonciniverdi-blocks/src/squadriglia/render.php
+++ plugins/guidonciniverdi-blocks/src/squadriglia/render.php
@@ -2,6 +2,6 @@
 $id = get_the_ID();
 $squadriglia = get_post_meta( $id, 'squadriglia', true );
 ?>
-<p <?php echo get_block_wrapper_attributes(); ?>>
+<h1 <?php echo get_block_wrapper_attributes(); ?>>
 	<?php echo esc_html( $squadriglia ); ?>
-</p>
+</h1>
